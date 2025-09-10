f6250b37779fc67541ed9430be0cadca3f2c55aa
Florian <dark.joosh@gmail.com>
2025-09-10 12:48:56 +0200

    Setting the applayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,29 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
 import Sidebar from "./Sidebar.jsx";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 63.4rem;
+`;
 
 const AppLayout = () => {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
-      <main className="main">
+      <Main className="main">
         <Outlet />
-      </main>
-    </div>
+      </Main>
+    </StyledAppLayout>
   );
 };
 export default AppLayout;
